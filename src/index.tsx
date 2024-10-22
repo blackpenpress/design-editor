4b47e00d66761b67377b2c2fa4c00f3bc99f5b9b
Tom <tom@Toms-MBP.lan>
2024-10-22 21:24:13 +0100

    edit index

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,56 @@
-import ReactDOM from 'react-dom'
-import AppContainer from './containers/AppContainer'
-import reportWebVitals from './reportWebVitals'
-import { AppProvider } from './contexts/app/AppContext'
-import './i18n/index'
-import { ThemeProvider } from 'theme-ui'
-import theme from './theme'
-import 'focus-visible/dist/focus-visible'
-import './styles/styles.scss'
-import { EditorProvider } from './uibox'
+// import ReactDOM from 'react-dom'
+// import AppContainer from './containers/AppContainer'
+// import reportWebVitals from './reportWebVitals'
+// import { AppProvider } from './contexts/app/AppContext'
+// import './i18n/index'
+// import { ThemeProvider } from 'theme-ui'
+// import theme from './theme'
+// import 'focus-visible/dist/focus-visible'
+// import './styles/styles.scss'
+// import { EditorProvider } from './uibox'
 
-ReactDOM.render(
+// ReactDOM.render(
+//   <AppProvider>
+//     <ThemeProvider theme={theme}>
+//       <EditorProvider>
+//         <AppContainer />
+//       </EditorProvider>
+//     </ThemeProvider>
+//   </AppProvider>,
+//   document.getElementById('root')
+// )
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals()
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppContainer from './containers/AppContainer';
+import reportWebVitals from './reportWebVitals';
+import { AppProvider } from './contexts/app/AppContext';
+import './i18n/index';
+import { ThemeProvider } from 'theme-ui';
+import theme from './theme';
+import 'focus-visible/dist/focus-visible';
+import './styles/styles.scss';
+import { EditorProvider } from './uibox';
+
+const DesignEditor = () => (
   <AppProvider>
     <ThemeProvider theme={theme}>
       <EditorProvider>
         <AppContainer />
       </EditorProvider>
     </ThemeProvider>
-  </AppProvider>,
-  document.getElementById('root')
-)
+  </AppProvider>
+);
+
+export default DesignEditor;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
